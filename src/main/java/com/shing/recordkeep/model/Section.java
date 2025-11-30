bd4ca0aef7e8dea75b4733d0256ce1f114094bf7
Keiaa <confinedrose@duck.com>
2025-11-30 18:56:56 +0800

    refactor: added unique constraint for section entity and tvl for the strand options

--- src/main/java/com/shing/recordkeep/model/Section.java
+++ src/main/java/com/shing/recordkeep/model/Section.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
@@ -15,6 +17,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"gradeLevel", "strand", "sectionName"}))
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
